d6444f35afd8b9a5e79d78bd3b7504a3fef00568
Esdras <piririsamuel23@gmail.com>
2026-09-19 12:12:56 -0600

    Actualizar base de datos completa con DDL de tablas y secuencias Oracle

--- base de datos/SIBank.sql
+++
@@ -1,5 +0,0 @@
-SELECT u.id_usuario, u.username, u.id_cliente, r.nombre AS rol, c.nombres, c.apellidos
-FROM Usuarios u
-JOIN Roles r ON r.id_rol = u.id_rol
-LEFT JOIN Clientes c ON c.id_cliente = u.id_cliente
-WHERE u.username = 'juanperez';
